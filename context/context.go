33358dd4071702553b1c17fc7c51f0239af8210e
Dao Duy Tung <teo2992001@gmal.com>
2024-02-15 15:12:13 +0700

    Initial commit

--- context/context.go
+++ context/context.go
@@ -6,20 +6,18 @@ import (
 	"github.com/Hustle299/Project-0/models"
 )
 
-// the key has 2 component : type and value, if we create a private type even though underlying is a string
-// it will be different than using just a string . string type != private key type
 type privateKey string
 
 const (
 	userKey privateKey = "user"
 )
 
-// function that accepts an existing context and a user, and then returns a new context with that user set as a value.
+// function that nhan 1 context da co va 1 user sau do tra ve context voi user la value moi
 func WithUser(ctx context.Context, user *models.User) context.Context {
 	return context.WithValue(ctx, userKey, user)
 }
 
-// function to lookup an user with given context
+// function de tim 1 user voi context da co
 func User(ctx context.Context) *models.User {
 	if temp := ctx.Value(userKey); temp != nil {
 		if user, ok := temp.(*models.User); ok {
@@ -28,4 +26,3 @@ func User(ctx context.Context) *models.User {
 	}
 	return nil
 }
-
